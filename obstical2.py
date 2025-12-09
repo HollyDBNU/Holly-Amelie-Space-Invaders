41b6f50a0de5b9d4d86fd3b672b68c2a5b8e5499
HollyDBNU <22503136@bnu.ac.uk>
2025-12-09 11:58:12 +0000

    Update obstical2.py

--- obstical2.py
+++ obstical2.py
@@ -30,13 +30,12 @@ grid = [
 [1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1]]
 
 class Obstacle:
-    def __init__(self, x, y):
-        self.blocks_group = pygame.sprite.Group()
-        # looping by index in a way that fails for empty rows
-        for row in range(len(grid)):
-            for column in range(len(grid[0])):
-                if grid[row][column] == 1:
-                    pos_x = x + column * 3
-                    pos_y = y + row * 3
-                    block = Block(pos_x, pos_y)
-                    self.blocks_group.add(block)
+	def __init__(self, x, y):
+		self.blocks_group = pygame.sprite.Group()
+		for row in range(len(grid)):
+			for column in range(len(grid[0])):
+				if grid[row][column] == 1:
+					pos_x = x + column * 3
+					pos_y = y + row * 3
+					block = Block(pos_x, pos_y)
+					self.blocks_group.add(block)
