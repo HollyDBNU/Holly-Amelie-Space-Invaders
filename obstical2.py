cdde83c0ab59fabf82cc301d24c05fc8e079fc17
HollyDBNU <22503136@bnu.ac.uk>
2025-12-09 11:56:05 +0000

    Update obstical2.py

--- obstical2.py
+++ obstical2.py
@@ -4,13 +4,16 @@ import pygame
 CELL_SIZE = 6  # size of each block cell (pixel width & height)
 BLOCK_COLOR = (243,216,63)  # colour of the obstical
 
+# ---------- Block Sprite (single obstacle cell) ----------
 class Block(pygame.sprite.Sprite):
     def __init__(self, x, y):
-        super().__init__() 
-        # originally left as None -> nothing to draw
-        self.image = None  
-        # rect not defined, pygame can't blit this sprite
-        # self.rect = None
+        super().__init__()
+        # create a Surface so Pygame can render the block
+        self.image = pygame.Surface((CELL_SIZE, CELL_SIZE))
+        self.image.fill((243, 216, 63))  # colour
+        # set rect for positioning and collision
+        self.rect = self.image.get_rect(topleft=(x, y))
+
 
 
     [0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,0],
