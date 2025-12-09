71a4f7113167e87b397d69c4c393167cac25c5e4
HollyDBNU <22503136@bnu.ac.uk>
2025-12-09 11:19:25 +0000

    Refactor Alien class and update movement logic

--- alien.py
+++ alien.py
@@ -1,7 +1,9 @@
 import pygame
 
+# Alien Sprite
 class Alien(pygame.sprite.Sprite):
-    def __init__(self,color,x,y):
+    # Alien Setup
+	def __init__(self,color,x,y):
         super().__init__()
         file_path = 'Graphics/' + color + '.png'
         self.image = pygame.image.load(file_path).convert_alpha()
@@ -10,10 +12,10 @@ class Alien(pygame.sprite.Sprite):
         if color == 'red' : self.value = 100
         elif color == 'green' : self.value = 200
         else: self.value = 300
-
+# Alien Movement Update
     def update(self, direction):
         self.rect.x += direction
-
+# Mystery Ship Sprite (spawns from a random side and moves across screen)
 class MysteryShip(pygame.sprite.Sprite):
 	def __init__(self, screen_width, offset):
 		super().__init__()
@@ -28,7 +30,7 @@ class MysteryShip(pygame.sprite.Sprite):
 			self.speed = -3
 
 		self.rect = self.image.get_rect(topleft = (x, 90))
-
+# Mystery Ship Movement & Auto-Despawn
 	def update(self):
 		self.rect.x += self.speed
 		if self.rect.right > self.screen_width + self.offset/2:
